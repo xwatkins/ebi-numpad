f54699c179442440c641b6b02b066b96020b3454
Xavier Watkins <xavier.watkins@gmail.com>
2019-07-10 21:39:44 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [code, setCode] = useState("");
+
+  const addNumberToCode = number => {
+    setCode(`${code}${number}`);
+  };
+
+  const clear = () => {
+    setCode("");
+  };
+
+  const deleteLast = () => {
+    if (code.length > 0) {
+      setCode(code.substring(0, code.length - 1));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Code">{code ? code : "Your number"}</div>
+      <div className="NumPad">
+        {Array(3)
+          .fill(3)
+          .map((row, index) => (
+            <div className="NumRow">
+              {Array(3)
+                .fill(3)
+                .map((col, index2) => {
+                  const number = index * 3 + 1 * index2 + 1;
+                  return (
+                    <button onClick={() => addNumberToCode(number)}>
+                      {number}
+                    </button>
+                  );
+                })}
+            </div>
+          ))}
+
+        <div className="NumRow">
+          <button onClick={() => clear()}>CLR</button>
+          <button onClick={() => addNumberToCode(0)}>0</button>
+          <button onClick={() => deleteLast()}>DEL</button>
+        </div>
+      </div>
     </div>
   );
 }
