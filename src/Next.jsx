cc8a0f3a54bb628cf825dfe9715361a0325bdc5f
Xavier Watkins <xavier.watkins@gmail.com>
2019-08-28 15:00:19 +0100

    Add music

--- src/Next.jsx
+++ src/Next.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from "react";
 import { AppDispatch, baseUrl } from "./App";
 import Axios from "axios";
+import drumRoll from "./sounds/drum_roll_norm.mp3";
 
 const Next = () => {
   const dispatch = useContext(AppDispatch);
@@ -17,6 +18,9 @@ const Next = () => {
         <button type="button" onClick={() => handleClick()}>
           Next
         </button>
+        <div style={{ marginTop: "2rem" }}>
+          <audio src={drumRoll} controls autoPlay />
+        </div>
       </div>
     </Fragment>
   );
